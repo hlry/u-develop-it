cd78536714b68ad49aa8cbc52193146f1645ddd7
Hilary Richardson <hilary.richardson@gmail.com>
2021-02-27 14:04:18 -0800

    improving candidate deletion and creation functionality in server

--- server.js
+++ server.js
@@ -30,13 +30,25 @@ db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
     console.log(row);
 });
 
-// Delete a candidate
-db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function (err, result) {
     if (err) {
         console.log(err);
     }
-    console.log(result, this, this.changes);
+    console.log(result, this.lastID);
 });
+
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
