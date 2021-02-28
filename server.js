4fbdff5324169305b0050601cb86d45e22599985
Hilary Richardson <hilary.richardson@gmail.com>
2021-02-27 16:35:27 -0800

    readability edits

--- server.js
+++ server.js
@@ -54,15 +54,29 @@ app.get('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function (err, result) {
-    if (err) {
-        console.log(err);
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
     }
-    console.log(result, this.lastID);
+
+    const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use this
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
 });
 
 // Delete a candidate
@@ -80,30 +94,6 @@ app.delete('/api/candidate/:id', (req, res) => {
             changes: this.changes
         });
     });
-});
-
-// Create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
-        VALUES (?,?,?)`;
-        const params = [body.first_name, body.last_name, body.industry_connected];
-
-        // ES5 function, not arrow function, to use `this`
-        db.run(sql, params, function (err, result) {
-            if (err) {
-                res.status(400).json({ error: err.message });
-                return;
-            }
-
-            res.json({
-                message: 'success',
-                data: body,
-                id: this.lastID
-            });
-        });
-    }
 });
 
 // Default response for any other request(Not Found) Catch all
